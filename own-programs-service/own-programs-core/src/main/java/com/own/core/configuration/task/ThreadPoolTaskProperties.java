be18d9859b6aedd38ceede52b7cd3e191e00f0aa
展望 <@DESKTOP-VHJ2N25>
2018-11-16 17:13:43 +0800

    多线程测试

--- own-programs-service/own-programs-core/src/main/java/com/own/core/configuration/task/ThreadPoolTaskProperties.java
+++ own-programs-service/own-programs-core/src/main/java/com/own/core/configuration/task/ThreadPoolTaskProperties.java
@@ -1,9 +1,7 @@
 package com.own.core.configuration.task;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.stereotype.Component;
 
-//@Component
 @ConfigurationProperties(prefix="thread.pool.task")
 public class ThreadPoolTaskProperties {
 
