7517f4f2476b016c71d9ace817c845f4722360f5
展望 <@DESKTOP-VHJ2N25>
2019-07-09 15:59:09 +0800

    启动获取token线程

--- own-programs-web/src/main/java/com/own/runner/MyApplicationRunner.java
+++ own-programs-web/src/main/java/com/own/runner/MyApplicationRunner.java
@@ -4,14 +4,16 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.own.core.utils.GetAccessTokenThread;
+
 @Component
 public class MyApplicationRunner implements ApplicationRunner{
 
 	@Override
 	public void run(ApplicationArguments arg0) throws Exception {
 		// TODO Auto-generated method stub
-		//启动定时获取access_token的线程
-		//new Thread(new GetAccessTokenThread()).start();//获取微信access_token
+//		启动定时获取access_token的线程
+		new Thread(new GetAccessTokenThread()).start();//获取微信access_token
 	}
 
 }
