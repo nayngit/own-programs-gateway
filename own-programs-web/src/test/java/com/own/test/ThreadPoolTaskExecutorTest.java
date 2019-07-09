6a9f43faed473d535ae926653860b60a122c10e4
展望 <@DESKTOP-VHJ2N25>
2019-07-09 16:40:12 +0800

    什么鬼

--- own-programs-web/src/test/java/com/own/test/ThreadPoolTaskExecutorTest.java
+++ own-programs-web/src/test/java/com/own/test/ThreadPoolTaskExecutorTest.java
@@ -15,37 +15,37 @@ import com.own.core.configuration.task.MyThreadPoolTaskExecutor;
 
 public class ThreadPoolTaskExecutorTest extends BaseTest{
 
-//	@Resource
-//	private MyThreadPoolTaskExecutor myThreadPoolTaskExecutor;
+	@Resource
+	private MyThreadPoolTaskExecutor myThreadPoolTaskExecutor;
 	
-//	@Test
-//	public void test01(){
-//		
-//		for (int i = 0; i < 10; i++) {
-//			SpringThread s = new SpringThread(i);
-//			myThreadPoolTaskExecutor.execute(s);
-//		}
-//		
-//		System.out.println("main process is finish .....");
-//		
-//		try {
-//			System.in.read();
-//		} catch (IOException e) {
-//			throw new RuntimeException(e);
-//		}
-//	}
-//	
-//	@Test
-//	public void test02(){
-//		myThreadPoolTaskExecutor.execute(new Runnable() {
-//			
-//			@Override
-//			public void run() {
-//				System.out.println("2222222222222");
-//				
-//			}
-//		});
-//	}
+	@Test
+	public void test01(){
+		
+		for (int i = 0; i < 10; i++) {
+			SpringThread s = new SpringThread(i);
+			myThreadPoolTaskExecutor.execute(s);
+		}
+		
+		System.out.println("main process is finish .....");
+		
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Test
+	public void test02(){
+		myThreadPoolTaskExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("2222222222222");
+				
+			}
+		});
+	}
 	
 	
 }
